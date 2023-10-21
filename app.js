fcc0030b89b27fae224a880599f156d922082a62
Aman <amank3125@gmail.com>
2023-10-21 21:19:49 +0530

    Added dynamic name & UID  via URL parameter

--- app.js
+++ app.js
@@ -22,5 +22,15 @@ var kinet = new Kinet({
     kinet.animate('y', event.clientY - window.innerHeight/2);
   });
 
-
-  
+console.clear();
+const url = window.location;
+let _header = document.querySelector('.header');
+let _uid1 = document.querySelector('.uid1');
+let _uid2 = document.querySelector('.uid2');
+let prstring = url.search.split("?")[1];
+let prArr = prstring.split("&")
+let empName = prArr[0].replace("empName=","").replace(/%20/g," ");
+let uid = prArr[1].replace("uid=","");
+_header.innerHTML = `Thank You ${empName}!`
+_uid1.innerHTML = `#${uid}`
+_uid2.innerHTML = `#${uid}`
